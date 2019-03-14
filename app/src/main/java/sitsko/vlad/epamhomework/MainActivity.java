50feb2370ff60e65e2b8ee27e969683973e07d72
Vlad Sitsko <vlad.sitsko@gmail.com>
2019-03-15 02:30:27 +0300

    task 2: added VK page

--- app/src/main/java/sitsko/vlad/epamhomework/MainActivity.java
+++ app/src/main/java/sitsko/vlad/epamhomework/MainActivity.java
@@ -1,7 +1,9 @@
 package sitsko.vlad.epamhomework;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.vk_open_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VkProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/sitsko/vlad/epamhomework/VkProfileActivity.java
+++ app/src/main/java/sitsko/vlad/epamhomework/VkProfileActivity.java
@@ -5,19 +5,28 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class VkProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vk_profile_activity);
+        setContentView(R.layout.activity_vk_profile);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_nav_view);
+        BottomNavigationView navigation = findViewById(R.id.bottom_nav_view);
         navigation.setSelectedItemId(R.id.navigation_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+
+        return true;
     }
 }
